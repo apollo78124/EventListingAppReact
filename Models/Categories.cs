837aa1c1a63a88136c545e3ebce1321e283a19f8
apollo78124 <98apollo@naver.com>
2020-08-29 04:14:08 -0700

    models added

---
+++ Models/Categories.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventReactApp.Models
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Events = new HashSet<Events>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Events> Events { get; set; }
+    }
+}

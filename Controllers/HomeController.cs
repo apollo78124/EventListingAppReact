944d207e9557f0430a923dd6bc287bd4fc35b17d
apollo78124 <98apollo@naver.com>
2020-08-29 13:43:18 -0700

    Pull mock data

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,9 +10,43 @@ namespace EventReactApp.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private static readonly IList<CommentModel> _comments;
+
+        static HomeController()
+        {
+            _comments = new List<CommentModel>
+            {
+                new CommentModel
+                {
+                    Id = 1,
+                    Author = "Daniel Lo Nigro",
+                    Text = "Comment from Controller"
+                },
+                new CommentModel
+                {
+                    Id = 2,
+                    Author = "Pete Hunt",
+                    Text = "TComment safeffrom Controller"
+                },
+                new CommentModel
+                {
+                    Id = 3,
+                    Author = "Jordan Walke",
+                    Text = "TComment safeffrom Controller"
+                },
+            };
+        }
+
+        public ActionResult Index()
         {
             return View();
+        }
+
+        [Route("comments")]
+        [ResponseCache(Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Comments()
+        {
+            return Json(_comments);
         }
 
         public IActionResult Privacy()
--- wwwroot/js/tutorial.jsx
+++ wwwroot/js/tutorial.jsx
@@ -1,9 +1,63 @@
-﻿class CommentBox extends React.Component {
+﻿const data1 = [
+    { id: 1, author: 'Daniel ssLo Nigro', text: 'hihihihihihihih' },
+    { id: 2, author: 'Pete dadHunt', text: 'cocococococococo' },
+    { id: 3, author: 'Jordaaan Walke', text: 'aaaaaaaaaaaaaaaaaaaaa' },
+];
+
+function createRemarkable() {
+    var remarkable =
+        'undefined' != typeof global && global.Remarkable
+            ? global.Remarkable
+            : window.Remarkable;
+
+    return new remarkable();
+}
+
+class Comment extends React.Component {
+    rawMarkup() {
+        const md = new Remarkable();
+        const rawMarkup = md.render(this.props.children.toString());
+        return { __html: rawMarkup };
+    }
     render() {
         return (
-            <div className="commentBox">Hello, world! I am a CommentBox.</div>
+            <div className="comment">
+                <h2 className="commentAuthor">{this.props.author}</h2>
+                <span dangerouslySetInnerHTML={this.rawMarkup()} />
+            </div>
         );
     }
 }
 
-ReactDOM.render(<CommentBox />, document.getElementById('content'));
+class CommentList extends React.Component {
+    render() {
+        const commentNodes = this.props.data.map(comment => (
+            <Comment author={comment.author} key={comment.id}>
+                {comment.text}
+            </Comment>
+        ));
+        return <div className="commentList">{commentNodes}</div>;
+    }
+}
+
+class CommentForm extends React.Component {
+    render() {
+        return (
+            <div className="commentForm">Comment Form</div>
+        );
+    }
+}
+
+class CommentBox extends React.Component {
+    render() {
+        return (
+            <div className="commentBox">
+                <h1>Comments</h1>
+                <CommentList data={this.props.data} />
+                <CommentForm />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<CommentBox data={data1} />, document.getElementById('content'));
